8cc36524616404236aae5d64f46ffdd1b6f5b79c
jaynamiproductions <jasonlee.yn@gmail.com>
2023-07-27 03:03:55 -0400

    typo

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from views import views
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sdfsdfsdfs'
 app.register_blueprint(views,url_prefix='/')
 
 if __name__ == '__main__':
