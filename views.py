6789719720d76b7976b25b92ffb8b6e276749af6
jaynamiproductions <jasonlee.yn@gmail.com>
2023-07-27 03:02:53 -0400

    typo

--- views.py
+++ views.py
@@ -7,7 +7,6 @@ api = 'https://data.cms.gov/data-api/v1/dataset/f1a8c197-b53d-4c24-9770-aea5d5a9
 resp = requests.get(api).json()
 df = pd.DataFrame(resp)
 
-
 views = Blueprint(__name__,'views')
 
 @views.route('/', methods=['GET','POST'])
