e7a6e8f36e26ec5b5b8ceb66506c118e43fc8a97
jaynamiproductions <jasonlee.yn@gmail.com>
2023-08-03 01:19:41 -0400

    new features

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from views import views
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sdgssdgwes'
 app.register_blueprint(views,url_prefix='/')
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 import requests 
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -14,7 +14,8 @@ def home():
     if request.method == 'POST':
         state = request.form.get('state')
         if len(state) < 1:
-            return render_template('error.html')
+            flash('Error. No selection made. Select a location to view provider information.',category='error')
+            return render_template('home.html')
         else:
             st = df[df['STATE']==state]
             col = st.shape[0]
@@ -36,6 +37,32 @@ def faq():
 def about():
     return render_template('about.html')
 
-@views.route('/npi-validation')
-def npi():
-    return render_template('npi.html')
+@views.route('/npi-validation',methods=['GET','POST'])
+def check():
+    if request.method == 'POST':
+        npi = request.form.get('npi')
+        if len(npi) < 1:
+            flash('No entry made.',category='error') 
+            
+        elif len(npi) != 10:
+            flash('NPI is invalid.',category='error')
+        elif npi.startswith('1') or npi.startswith('2'):
+            new = npi[-2::-1]
+            test = ''
+            for i in range(len(new)):
+                if i % 2 == 0:
+                    test += str(int(new[i])*2)
+                else:
+                    test += new[i]
+            total = map(int, str(test))
+            final = sum(total)+24
+            check = 10 - (final%10)
+            if check == int(npi[-1]):
+                flash('NPI is valid.',category='success')
+            else:
+                flash('NPI is invalid.',category='error')
+        if len(npi) != 15:
+            flash('NPI is invalid.',category='error')
+        return render_template('npi.html',num=npi)
+    else:
+        return render_template('npi.html')
